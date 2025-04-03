9bcc7eb15f6503266759f1a1d6766475e0719993
n1ckg4m3s <nico.ag.03@hotmail.com>
2025-04-03 15:55:14 -0300

    Adicionar strutura inicial do projeto

---
+++ src/pages/detalhes_projetos/index.tsx
@@ -0,0 +1,10 @@
+const detalhe_projetos = () => {
+    return (
+        <div>
+        <h1>projeto</h1>
+        <p>detalhe</p>
+        </div>
+    );
+}
+
+export default detalhe_projetos;
---
+++ src/pages/main/index.tsx
@@ -0,0 +1,10 @@
+const mainPortifolio = () => {
+    return (
+        <div>
+            <h1>Portifolio</h1>
+            <p>Portifolio</p>
+        </div>
+    );
+}
+
+export default mainPortifolio;
